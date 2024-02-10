b6232621539a9f4032b724bb7c0e2102372c379a
Jaehyung Park <jay@Jaehyungs-MacBook-Pro.local>
2024-02-09 17:03:29 -0700

    updates

--- users/views.py
+++ users/views.py
@@ -91,9 +91,15 @@ class LogIn(APIView):
         )
         if user:
             login(request, user)
-            return Response({"ok": "Welcome!"})
+            return Response(
+                {"ok": "Welcome!"},
+                status=status.HTTP_200_OK,
+            )
         else:
-            return Response({"error": "wrong password"})
+            return Response(
+                {"error": "wrong password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
 class LogOut(APIView):
